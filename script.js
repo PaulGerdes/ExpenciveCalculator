5513837b0d24fa6a0d7c5f2bfe0f83032578cbcf
Paul Gerdes <paul-gerds@gmx.net>
2023-09-26 20:34:06 +0200

    fixes

--- script.js
+++ script.js
@@ -2,25 +2,54 @@ let insert;
 let plusOderMinus;
 let nummerEintrag = 1;
 let issuancetype;
+let knopfZahl;
+let vermogen = 0;
+
+function VermögensVerwaltung(einkommen) {
+  console.log(einkommen);
+  vermogen = vermogen + einkommen;
+  document.getElementById("#incomeDashboard").innerHTML = vermogen;
+}
 
 function setIncome() {
-  insert = document.querySelector("#income").value;
-  console.log(insert);
-  NeuerEintrag(insert, "plus");
-  document.querySelector("#income").value = null;
-  document.getElementById("incomeDashboard").innerHTML = insert;
+  if (!document.querySelector("#income").value) {
+    document.querySelector("#income").value = "Du musst eine zahl eingeben";
+  }
+  if (document.querySelector("#income").value > null) {
+    insert = document.querySelector("#income").value;
+    VermögensVerwaltung(insert.value);
+    neuerEintrag(insert, "plus");
+    document.querySelector("#income").value;
+  }
 }
+
 function setissuance() {
-  nummerEintrag = nummerEintrag + 1;
-  insert = document.querySelector("#issuancevalue").value;
-  issuancetype = document.querySelector("#issuancetype");
-  console.log(insert);
-  NeuerEintrag(insert, "minus", issuancetype);
-  document.querySelector("#issuancetype").value = null;
-  document.querySelector("#issuancevalue").value = null;
-  document.getElementById("issuanceDashbaord").innerHTML = insert;
+  if (!document.querySelector("#issuancevalue").value) {
+    document.querySelector("#issuancevalue").value =
+      "Du musst eine zahl eingeben";
+  } else {
+    if (!document.querySelector("#issuancetype").value) {
+      document.querySelector("#issuancetype").value =
+        "Du musst einen Grund eingeben";
+    }
+  }
+  if (
+    document.querySelector("#issuancevalue").value > null &&
+    document.querySelector("#issuancetype").value > null
+  ) {
+    insert = document.querySelector("#issuancevalue").value;
+    issuancetype = document.querySelector("#issuancetype");
+
+    neuerEintrag(insert, "minus", issuancetype);
+    document.querySelector("#issuancetype").value;
+    document.querySelector("#issuancevalue").value;
+    document.getElementById("issuanceDashbaord").innerHTML = insert;
+  }
 }
-function NeuerEintrag(insert, plusOderMinus, issuancetype) {
+
+function neuerEintrag(insert, plusOderMinus, issuancetype) {
+  nummerEintrag = nummerEintrag + 1;
+  console.log(nummerEintrag);
   var einzufuegendesObjekt = document.createElement("div");
 
   einzufuegendesObjekt.id = "eintrag" + nummerEintrag;
@@ -34,9 +63,26 @@ function NeuerEintrag(insert, plusOderMinus, issuancetype) {
   document.getElementById("eintrag" + nummerEintrag).appendChild(wert2);
 
   if (issuancetype == null) {
+    var wert3 = document.createElement("p");
+    wert3.innerHTML = null;
+    document.getElementById("eintrag" + nummerEintrag).appendChild(wert3);
   } else {
     var wert3 = document.createElement("p");
     wert3.innerHTML = issuancetype.value;
     document.getElementById("eintrag" + nummerEintrag).appendChild(wert3);
   }
+  var wert4 = document.createElement("button");
+  knopfzahl = "button" + nummerEintrag;
+  wert4.id = nummerEintrag;
+  wert4.innerHTML = "x";
+  wert4.setAttribute("onclick", "deleteblock(this.id)");
+
+  document.getElementById("eintrag" + nummerEintrag).appendChild(wert4);
+}
+
+function deleteblock(zahl) {
+  console.log(zahl);
+  let testid = "eintrag" + zahl;
+  datei = document.getElementById(testid);
+  datei.remove();
 }
